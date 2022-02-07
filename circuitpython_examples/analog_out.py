9fdd59d3988bf8044c728fda0f7c9d51543a2bc4
nepnepssis-khoa <khngo22@ssis.edu.vn>
2022-02-07 10:35:58 +0700

    Update analog_out.py

--- circuitpython_examples/analog_out.py
+++ circuitpython_examples/analog_out.py
@@ -8,4 +8,5 @@ while True:
     # Count up from 0 to 65535, with 64 increment
     # which ends up corresponding to the DAC's 10-bit range
     for i in range(0, 65535, 64):
+        # analog output incrementing
         analog_out.value = i
